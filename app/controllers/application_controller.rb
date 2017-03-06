22881f544815795909da0009b8b45c4ed0f7f9b8
Adam Cooke <me@adamcooke.io>
2017-03-06 16:55:49 +0000

    catch authie session errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
+require 'authie/session'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include AjaxSubmissions
 
+  rescue_from Authie::Session::InactiveSession, :with => :auth_session_error
+  rescue_from Authie::Session::ExpiredSession, :with => :auth_session_error
+  rescue_from Authie::Session::BrowserMismatch, :with => :auth_session_error
+
   private
+
+  def auth_session_error
+    # TODO: update this path to be appropriate for this application
+    redirect_to root_path
+  end
 
   def page_title
     @page_title ||= ['LlamaApp']
