d4a780053e2eea61672f937f71c50033d974fbb8
Adam Cooke <me@adamcooke.io>
2018-03-20 17:39:31 +0000

    upgrade things

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,8 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
+workers ENV.fetch("PUMA_WORKERS") { 2 }.to_i
+
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
